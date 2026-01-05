9d0838828611fd209775e065d3920bfec10259b9
Anusree11 <anusree1598@gmail.com>
2026-01-06 02:26:39 +0530

    Partial functions with database

--- app.py
+++ app.py
@@ -1,13 +1,42 @@
 from flask import Flask, request, jsonify
 import sqlite3
 
+def get_db_connection():
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS customers (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS accounts (
+            account_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            customer_id INTEGER,
+            balance INTEGER,
+            FOREIGN KEY (customer_id) REFERENCES customers (id)
+        )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
 
 
 app = Flask(__name__)
 
 # temporary in-memory storage
-customers = []
-accounts = []
+
 
 @app.route("/", methods=["GET"])
 def home():
@@ -17,45 +46,64 @@ def home():
 def create_customer():
     data = request.get_json()
 
-    customer = {
-        "id": len(customers) + 1,
-        "name": data.get("name"),
-        "email": data.get("email")
-    }
+    conn=get_db_connection()
+    cursor= conn.cursor()
 
-    customers.append(customer)
+    cursor.execute(
+        "INSERT INTO customers (name, email) VALUES (?, ?)",
+        (data.get("name"), data.get("email"))
+    )
+
+    conn.commit()
+    customer_id = cursor.lastrowid
+    conn.close()
 
     return jsonify({
-        "message": "Customer created successfully",
-        "customer": customer
+        "id": customer_id,
+        "name": data.get("name"),
+        "email": data.get("email")
     }), 201
 
 @app.route("/customers", methods=["GET"])
 def get_customers():
-    return jsonify(customers), 200
+    conn=get_db_connection()
+    customers=conn.execute("SELECT * FROM customers").fetchall()
+    conn.close()
+
+    return jsonify([dict(c) for c in customers]), 200
 
 @app.route("/accounts", methods=["POST"])
 def create_account():
     data=request.get_json()
     customer_id= data.get("customer_id")
 
-    customer_exists = any(c["id"]== customer_id for c in customers)
+    conn=get_db_connection()
+    cursor = conn.cursor()
+
+    customer = cursor.execute("Select * FROM customers WHERE id= ?",
+                              (customer_id,)).fetchone()
     
-    if not customer_exists:
+    if not customer:
+        conn.close()
         return jsonify({"error": "Customer not found"}), 404
     
-    account = {
-        "account_id": len(accounts) + 1,
-        "customer_id": customer_id,
-        "balance":0
-    }
+    cursor.execute(
+        "INSERT INTO accounts (customer_id, balance) VALUES (?, ?)", (customer_id, 0)
+    )
 
-    accounts.append(account)
+    conn.commit()
+    account_id=cursor.lastrowid
+    conn.close()
 
     return jsonify({
-        "message": "Account created successfully",
-        "account": account
+        "message": "Account created succesfully",
+        "account": {
+        "account_id": account_id,
+        "customer_id": customer_id,
+        "balance": 0
+        }
     }), 201
+
 
 @app.route("/deposits", methods=["POST"])
 def deposit():
@@ -63,16 +111,34 @@ def deposit():
     account_id=data.get("account_id")
     amount=data.get("amount")
 
-    for account in accounts:
-        if account["account_id"] == account_id:
-            account["balance"]+=amount
+    conn= get_db_connection()
+    cursor= conn.cursor()
 
-            return jsonify ({
-                "message": "Deposit succesfully",
-                "balance": account["balance"]
-            }), 200
-        
-    return jsonify ({"error": "Account not found"}), 404
+    account= cursor.execute(
+        "SELECT * FROM accounts WHERE account_id=?", 
+        (account_id,)).fetchone()
+    
+    if not account:
+        conn.close()
+        return jsonify({
+            "error": "Account does not exists"
+        }), 404
+    
+    new_balance=account["balance"]+amount
+    
+    cursor.execute(
+        "UPDATE accounts SET balance =? WHERE account_id=?", (new_balance, account_id)
+
+    )
+
+    conn.commit()
+    conn.close()
+
+    return jsonify ({
+        "message": "Deposit succesful",
+        "balance": new_balance
+    }), 200
+
 
 @app.route("/withdraw", methods=["POST"])
 def withdraw():
@@ -93,5 +159,5 @@ def withdraw():
             }), 200
 
 if __name__ == "__main__":
-    
+    init_db()
     app.run(debug=True)
